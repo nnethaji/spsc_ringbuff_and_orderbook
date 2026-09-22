8946070b0de9921a2fe97c27074b363723c6abff
ikarfortn <753137nn@student.eur.nl>
2026-09-22 17:18:24 +0530

    Create parser.cpp
    - static assert size of buffer
    - ofstream needs to be ios::binary (to prevent \n issue with windows)
    - write takes ptr to raw bytes so cast start of m to const char*

---
+++ parser.cpp
@@ -0,0 +1,51 @@
+#include <algorithm>
+#include <cstdint>
+#include <chrono>
+#include <cstdlib>
+#include <fstream>
+#include <ios>
+// offset	      field	         type	     size
+// 0	        sequence_no 	 uint64_t	   8
+// 8	        timestamp_ns	 uint64_t	   8
+// 16	           price	     int64_t	   8
+// 24	          quantity	     uint32_t	   4
+// 28	           SYMBOL 	     uint16_t	   2
+// 30	           buy/sell	     char	       1
+// 31	           msg_type	     uint8_t	   1
+
+
+struct msg {
+    uint64_t sequence_no;
+    uint64_t timestamp_ns;
+    int64_t price;
+    uint32_t quantity;
+    uint16_t symbol_id;
+    char buy_sell;
+    uint8_t msg_type;
+};
+
+static_assert(sizeof(msg) == 32, "msg is not 32 bytes");
+
+void generate(const char* path, int n){
+
+    std::ofstream out(path,std::ios::binary);
+    for(int i = 0; i<n ; i++){
+        
+        msg m;
+        m.sequence_no = i+1;
+
+        std::chrono::system_clock::time_point arrival_time = std::chrono::system_clock::now();
+        m.timestamp_ns = std::chrono::duration_cast<std::chrono::nanoseconds>(
+            arrival_time.time_since_epoch()
+        ).count();
+
+        m.price = random();
+        m.quantity = rand();
+        m.symbol_id = rand()%1234;
+        m.buy_sell = (rand()%2 ==0)?'B':'S';
+        m.msg_type = rand()%3;
+        out.write(reinterpret_cast<const char*>(&m), sizeof(m));
+
+    }
+    
+}
