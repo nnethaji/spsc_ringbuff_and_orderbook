28676ba410eb90a57971951384bb02c4fcd3d1e3
ikarfortn <753137nn@student.eur.nl>
2026-09-23 16:26:36 +0530

    make producer a separate thread

--- main.cpp
+++ main.cpp
@@ -2,42 +2,45 @@
 #include <cstdint>
 #include <iostream>
 #include <thread>
-#include <vector>
 
 int main(){
-    SPSCQueue<msg, 8> lock_udaya_buffer;
-    std::vector<std::thread> threads;
-    
-    threads.emplace_back([&](){
-        for(int i=0; i<1000; i++){
-            msg m{};
-            m.sequence_no = i;
-            lock_udaya_buffer.push(m);
-        }
-        lock_udaya_buffer.shutdown();
-    });
-    
-    uint64_t i =0;
-    threads.emplace_back([&]() {
-        msg seq;
-        while(lock_udaya_buffer.pop(seq)){
-            if(seq.sequence_no != i){
-                std::cerr<< "out of order";
-            }
-            i++;
-            //std::this_thread::sleep_for(std::chrono::microseconds(1));
-        }
-    });
-    
+    // std::vector<std::thread> threads;
+    uint64_t count =0;
+    SPSCQueue<msg, 1024> lock_buffer;
 
-    for(auto& th:threads){
-        if(th.joinable()){
-            th.join();
-        }
-    }
-    if(i==1000){
-        std::cout<<"WORKS";
-    }
+    //generate dummy msg
+    generate("trialfile.txt", 10000);
+
+    //parser calls producer
+    std::thread producer([&]{ 
+        parse("trialfile.txt", lock_buffer); 
+    });
+
+    std::thread consumer([&]{ 
+        msg m; 
+        while (lock_buffer.pop(m)) 
+            ++count; 
+    });
+    
+    // threads.emplace_back([&]() {
+    //     msg seq;
+    //     while(lock_buffer.pop(seq)){
+    //         i++;
+    //         //std::this_thread::sleep_for(std::chrono::microseconds(1));
+    //     }
+    // });
+
+
+    // for(auto& th:threads){
+    //     if(th.joinable()){
+    //         th.join();
+    //     }
+    // }
+    producer.join();
+    consumer.join();
+    std::cout << (count == 10000 ? "WORKS" : "FAILED") << " count=" << count << "\n";
+
 
     return 0;
 }
+
